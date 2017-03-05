f0333fdbbcb194dcb12f99ca5e10277e77cff91a
dragonzonee <konglongdan34@163.com>
2017-03-05 23:57:36 +0800

    upload rating project, unfinished

---
+++ Rating/js/style.js
@@ -0,0 +1,19 @@
+function rateone(){
+	document.getElementClassName(one).style.background="#d52212"
+}
+
+function ratetwo(){
+	document.getElementClassName(two).style.background="red";
+}
+
+function ratethree(){
+	document.getElementClassName(two).style.background="red";
+}
+
+function ratefour(){
+	document.getElementClassName(two).style.background="red";
+}
+
+function ratefive(){
+	document.getElementClassName(two).style.background="red";
+}
