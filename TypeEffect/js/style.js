b29764669d7abc58c3956b92c227c780d2a11366
dragonzonee <konglongdan34@163.com>
2017-03-13 23:09:03 +0800

    upload the file of TypeEffect, unfinished

---
+++ TypeEffect/js/style.js
@@ -0,0 +1,25 @@
+
+var num=2;
+var tem_word;
+var timer;
+var sentence = document.getElementById("sentence");
+var length=sentence.innerHTML.length;
+function typeEffect(){
+  tem_word = sentence.innerHTML.substring(0,num);
+  sentence.innerHTML=tem_word;
+  if(num==length){
+    clearInterval(timer);
+  }else{
+    num++;
+  }
+  timer=setInterval("typeEffect()",1200);
+}
+
+/*function ifrepeat(){
+  if(num==length){
+    clearInterval(timer);
+  }else{
+    num++;
+  }
+}
+*/
